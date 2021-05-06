02f836eac818dbca99b832025e839ba1f3ba2e39
이효진 <billion109@naver.com>
2021-05-06 12:37:29 +0900

    social Login

--- src/main/java/com/example/noticeboard/web/IndexController.java
+++ src/main/java/com/example/noticeboard/web/IndexController.java
@@ -1,5 +1,7 @@
 package com.example.noticeboard.web;
 
+import com.example.noticeboard.config.auth.LoginUser;
+import com.example.noticeboard.config.auth.dto.SessionUser;
 import com.example.noticeboard.service.posts.PostsService;
 import com.example.noticeboard.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -8,14 +10,20 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
     private final PostsService postsService;
 
     @GetMapping("/")
-    public String index(Model model) {
-        model.addAttribute("posts",postsService.findAllDesc());
+    public String index(Model model, @LoginUser SessionUser user) {
+        model.addAttribute("posts", postsService.findAllDesc());
+
+        if (user != null) {
+            model.addAttribute("userName", user.getName());
+        }
         return "index";
     }
 
@@ -25,11 +33,11 @@ public class IndexController {
     }
 
     @GetMapping("/posts/update/{id}")
-    public String postsUpdate(@PathVariable Long id, Model model){
+    public String postsUpdate(@PathVariable Long id, Model model) {
         PostsResponseDto dto = postsService.findById(id);
         model.addAttribute("post", dto);
 
-        return  "posts-update";
+        return "posts-update";
     }
 
 }
